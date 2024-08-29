55dd0166d19a734b2876fc04440649fad5c8191e
DatHoang <8732206@gmail.com>
2024-08-29 12:09:08 +0900

    hello

---
+++ src/main/java/org/example/Time.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/time")
+public class Time extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Current Time: " + new SimpleDateFormat("HH:mm:ss dd/MM/yyyy").format(new Date()) + "</h1>");
+        out.println("</body></html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: HELO BABE
+  Date: 2024/08/27
+  Time: 2:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <style>
+        * {
+            text-align: center;
+        }
+
+        button {
+            margin-top: 10px;
+            padding: 20px;
+            font-size: 20px;
+            border-radius: 10px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<form action="time" method="get">
+    <button type="submit">Hiển thị thời gian hệ thống</button>
+</form>
+</body>
+</html>
